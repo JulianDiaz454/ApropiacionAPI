0f0f8374611f3ca24518ecccd7174e22200a50f6
Julian Diaz <torresanibal336@gmail.com>
2026-02-02 17:10:48 -0500

    Se realizan los ejercicios de post, creación de nuevos items

---
+++ peticiones/app2.js
@@ -0,0 +1,40 @@
+// Peticiones de tipo POST
+
+// Solicitud 4 - Crear una nueva publicación
+const SolicitudPOST = async () => {
+    const nuevo_objeto = await fetch(`http://localhost:3000/posts`, {
+        method: "POST", // Metodo de tipo POST
+        body: JSON.stringify({
+            "userId": 5,
+            "title": "Nuevo post creado para el usuario 5",
+            "body": "Post completo"
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    }) // Se genera la solicitud con dos argumentos, el fetch y el post
+
+    const objeto_data = await nuevo_objeto.json()
+    console.log(objeto_data);
+}
+
+// Solicitud 5 - Crear un nuevo comentario
+const SolicitudPOST_comentario = async () => {
+    const nuevo_comentario = await fetch(`http://localhost:3000/comments`, {
+        method: "POST", // Metodo de tipo POST
+        body: JSON.stringify({
+            "postId": 7540,
+            "title": "Comentario 19 del post 7540",
+            "body": "Ejemplo de un nuevo post"
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    }) // Se genera la solicitud con dos argumentos, el fetch y el post
+
+    const comentario_data = await nuevo_comentario.json()
+    console.log(comentario_data);
+}
+
+SolicitudPOST();
+SolicitudPOST_comentario();
