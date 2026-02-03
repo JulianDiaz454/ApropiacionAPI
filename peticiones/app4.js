5a766fa8e54d739f6c109c3733211ebf2614cc08
Julian Diaz <torresanibal336@gmail.com>
2026-02-02 22:29:41 -0500

    Se realiza la parte 4, petición de eliminar post

--- peticiones/app4.js
+++ peticiones/app4.js
@@ -2,9 +2,13 @@
 
 // Solicitud 8 - Eliminar información con el metodo DELETE
 const SolicitudDELETE = async (id) => {
-    fetch(`http://localhost:3000/posts/${id}`, {
+    const eliminar_post = await fetch(`http://localhost:3000/posts/${id}`, {
         method: "DELETE", // Este metodo elimina un objeto existente 
     })
+
+    const data_eliminar = await eliminar_post.json(); 
+    console.log(`Se elimino el post con el ID : ${id}\n ${data_eliminar}`);
 }
 
+// Llamamos a la función
 SolicitudDELETE(8);
